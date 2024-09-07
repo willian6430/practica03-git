9ca9d7077545cbba40f9d5d6800309cee8a96efd
willian6430 <74684940+willian6430@users.noreply.github.com>
2024-09-07 17:11:00 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personajes de Dragon Ball</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Personajes de Dragon Ball</h1>
+    <div class="small-card-container">
+        <?php include 'fetch_characters.php'; ?>
+    </div>
+</body>
+</html>
